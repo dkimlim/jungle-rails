4365d19908d96c2ac583a4979af10cbcd31963d8
Diane KL <dianekimlim@gmail.com>
2018-03-02 16:03:41 +0000

    Added functionality for ActionMailer to send confirmation email to dianekimlim@gmail.com. This is hardcoded in the Stripe client account in views and would need to be changed later to make it dynamical change to the user's email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+	default from: 'no-reply@jungle.com'
+
+	def confirmation_email(order)
+
+		mail(to: order.email, subject: 'Thank you for order #{order.id}')
+
+	end
 end
