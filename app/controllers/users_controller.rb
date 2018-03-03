dc589ea33f4953f0639378f4e2c5c501a476fad1
Diane KL <dianekimlim@gmail.com>
2018-03-03 23:24:05 +0000

    added error messages for signup/login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
   end
 
   def create
-	  user = User.new(user_params)
-	  if user.save
-	    session[:user_id] = user.id
+	  @user = User.new(user_params)
+	  if @user.save
+      flash[:success] = "Welcome to Jungle!"
+	    session[:user_id] = @user.id
 	    redirect_to '/'
 	  else
+      flash[:alert] = "Sign up did not work :("
 	    redirect_to '/signup'
     end
   end
